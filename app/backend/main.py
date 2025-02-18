7b52a2f85711d1a4675372d278142a69d81a2168
davidm <dm@salmonbusinesssolutions.com>
2025-02-18 08:35:35 +0300

    refactor main.py to enhance static file handling and include API router

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,31 +1,40 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, HTTPException
 from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 from app.backend.api.routes.api import router as api_router
 from app.backend.database.utils import initialize_database
 
 app = FastAPI(on_startup=[initialize_database])
 
-templates = Jinja2Templates(directory="app/frontend/customer_app/.web/_static")
 
-
-# Mount the "/static" directory for serving static files such as CSS, JavaScript, and images
-app.mount(
-    "/_next",
-    StaticFiles(directory="app/frontend/customer_app/.web/_static/_next"),
-    name="_next",
-)
-
-# Include the defined API routers into the application
+# Include the API router in the application.
+# This will mount all API endpoints defined in 'api_router'.
 app.include_router(api_router)
 
 
-@app.get("/", response_class=HTMLResponse, include_in_schema=False)
-async def index_page_resource(request: Request):
-    context = {
-        "request": request,
-    }
+# Define a subclass of StaticFiles to create a custom static file handler
+# that supports client-side routing in Single Page Applications (SPAs).
+class SPAStaticFiles(StaticFiles):
+    # Override the get_response method, which is responsible for retrieving
+    # static file responses for given paths.
+    async def get_response(self, path: str, scope):
+        try:
+            return await super().get_response(path, scope)
+        except (HTTPException, StarletteHTTPException) as ex:
+            if ex.status_code == 404:
+                return await super().get_response("index.html", scope)
+            else:
+                raise ex
 
-    return templates.TemplateResponse("index.html", context=context)
+
+# Mount the SPAStaticFiles instance at the root URL ("/").
+# This means any request not caught by other routes (like the API) will be handled here.
+# - 'directory' specifies where your built static files (from Next.js) reside.
+# - 'html=True' tells FastAPI to serve HTML files by default.
+# - 'name' is an identifier for this mounted application
+app.mount(
+    "/",
+    SPAStaticFiles(directory="app/frontend/customer_app/.web/_static/", html=True),
+    name="_next",
+)
