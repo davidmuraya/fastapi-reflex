f0362d4e8ec73699d2ce8ff14144e297b9811bfb
davidm <dm@salmonbusinesssolutions.com>
2025-02-18 09:38:42 +0300

    add about page with FastAPI routing and HTML template

--- app/backend/api/routes/api.py
+++ app/backend/api/routes/api.py
@@ -1,10 +1,15 @@
 from fastapi import APIRouter
 
+# FastAPI routes for APIs
 from app.backend.auth.routes import router as auth_routes
 from app.backend.customer.routes import router as customer_routes
 
+# An example of an existing route that renders some HTML
+from app.frontend.template_controllers.about import router as about_routes
+
 router = APIRouter()
 
-
+# Include the routers:
 router.include_router(customer_routes)
 router.include_router(auth_routes)
+router.include_router(about_routes)
---
+++ app/frontend/template_controllers/about.py
@@ -0,0 +1,20 @@
+"""
+This module contains the router for the 'about' page.
+This is to demonstrate FastAPI rendering HTML templates for an existing page/route.
+"""
+
+from fastapi import APIRouter, Request, status
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+router = APIRouter()
+templates = Jinja2Templates(directory="app/frontend/templates")
+
+
+@router.get("/about", response_class=HTMLResponse, include_in_schema=False)
+async def not_found_page_resource(request: Request):
+    return templates.TemplateResponse(
+        "about.html",
+        context={"request": request},
+        status_code=status.HTTP_404_NOT_FOUND,
+    )
