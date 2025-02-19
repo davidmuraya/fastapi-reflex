27b93b5887a156e47038d13bf23d3758d4043444
davidm <dm@salmonbusinesssolutions.com>
2025-02-19 16:33:12 +0300

    fix: update backend port and API URL to 8001 in rxconfig

--- app/frontend/customer_app/rxconfig.py
+++ app/frontend/customer_app/rxconfig.py
@@ -14,13 +14,13 @@ config = rx.Config(
     show_built_with_reflex=False,
     cors_allowed_origins=dev_origins,
     telemetry_enabled=False,
-    backend_port=8000,
+    backend_port=8001,
     frontend_port=3000,
     loglevel="debug",
     db_url=None,
     backend_host="127.0.0.1",
     gunicorn_worker_class="uvicorn.workers.UvicornH11Worker",
     gunicorn_workers=4,  # Set number of worker processes
-    api_url="http://127.0.0.1:8000",
+    api_url="http://127.0.0.1:8001",
     is_reflex_cloud=False,
 )
