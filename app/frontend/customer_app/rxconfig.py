a6031f443fb83db6353fb12b598c86517b8f92d0
davidm <dm@salmonbusinesssolutions.com>
2025-02-19 14:09:36 +0300

    update production CORS origins and configuration for deployment

--- app/frontend/customer_app/rxconfig.py
+++ app/frontend/customer_app/rxconfig.py
@@ -4,9 +4,7 @@ rxconfig.py contains the configuration for Reflex.
 
 import reflex as rx
 
-prod_origins = [
-    "http://127.0.0.1",
-]
+prod_origins = ["http://127.0.0.1", "https://demo.bima-mo.com"]
 
 dev_origins = ["*"]
 
@@ -14,11 +12,15 @@ dev_origins = ["*"]
 config = rx.Config(
     app_name="customer_data",
     show_built_with_reflex=False,
-    cors_allowed_origins=prod_origins,
+    cors_allowed_origins=dev_origins,
     telemetry_enabled=False,
     backend_port=8000,
     frontend_port=3000,
     loglevel="debug",
     db_url=None,
     backend_host="127.0.0.1",
+    gunicorn_worker_class="uvicorn.workers.UvicornH11Worker",
+    gunicorn_workers=4,  # Set number of worker processes
+    api_url="http://127.0.0.1:8000",
+    is_reflex_cloud=False,
 )
