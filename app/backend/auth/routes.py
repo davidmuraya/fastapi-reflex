80fb1e7f8a348aea71c4ff739d481b06c464780b
davidm <dm@salmonbusinesssolutions.com>
2025-02-20 14:44:57 +0300

    feat: enhance authentication routes with token generation and user status checks

--- app/backend/auth/routes.py
+++ app/backend/auth/routes.py
@@ -1,64 +1,101 @@
-from fastapi import APIRouter, Depends, HTTPException, status
-from pydantic import BaseModel
-from sqlmodel import Field, Session, SQLModel, select
+from datetime import datetime, timedelta
 
+from fastapi import APIRouter, Depends, HTTPException, Request, Response, status
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlmodel import Session
+
+from app.backend.auth.auth import (
+    ACCESS_TOKEN_EXPIRE_DAYS,
+    authenticate_user,
+    create_access_token,
+)
+from app.backend.auth.models import Token, UserLoginRequest, UserLoginResponse
 from app.backend.database.utils import get_session
 
-router = APIRouter(prefix="/api")
-
-
-# SQLModel setup
-class User(SQLModel, table=True):
-    id: int = Field(primary_key=True)
-    name: str
-    email: str
-    password: str
-
-
-class UserLoginRequest(BaseModel):
-    email: str
-    password: str
-
-
-class UserLoginResponse(BaseModel):
-    id: int
-    name: str
-    email: str
+router = APIRouter(prefix="/api/v1")
 
 
 @router.post("/auth/login", response_model=UserLoginResponse)
 def login_user(login_data: UserLoginRequest, session: Session = Depends(get_session)):
     """
-    This is just an example of how to retrieve a user from the database.
+    Authenticate a user by checking their username and password
 
     """
 
-    # exception:
-    user_not_found_exception = HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail="User not found. Please check the spelling.",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-
-    invalid_credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Invalid username or password.",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-
     # Get the user from the database
-    db_user = session.exec(select(User).where(User.email == login_data.email)).first()
+    db_user = authenticate_user(email=login_data.email, password=login_data.password)
 
-    # If the user doesn't exist, raise an exception
-    if not db_user:
-        raise user_not_found_exception
-
-    # You likely need to hash and verify the password instead of comparing it directly
-    if not db_user.password == login_data.password:
-        raise invalid_credentials_exception
+    # Create the access_token:
+    access_token = create_access_token(data={"sub": db_user.email}, scope="app")
 
     # Return the user data
     user_data_response = UserLoginResponse(
-        id=db_user.id, name=db_user.name, email=db_user.email
+        id=db_user.id, name=db_user.name, email=db_user.email, access_token=access_token
     )
     return user_data_response
+
+
+@router.post(
+    "/token", response_model=Token, tags=["Authentication"], summary="👤 Get token"
+)
+async def login_for_access_token(
+    response: Response,
+    request: Request,
+    form_data: OAuth2PasswordRequestForm = Depends(),
+):
+    """
+    # Endpoint to log in a user and provide an access token.
+
+    🛡️ This endpoint authenticates a user using their username and password, retrieves the user's IP address
+    and geo-location, and creates an access token if the credentials are correct and the user account is active.
+
+    ### Params:
+
+        🔓 username: The username you created the account with.
+        🔓 password: Your account password.
+
+    ### Returns:
+
+        Token: A Token object containing the access token, token type, expiry time, and success status.
+
+    ### Raises:
+
+        HTTPException: If the credentials are incorrect (401 Unauthorized) or the user account is inactive (403 Forbidden).
+    """
+
+    # Authenticate the user:
+    user = authenticate_user(form_data.username, form_data.password)
+
+    if not user:
+        response_error = f"Credentials Error. Incorrect username or password. User:{form_data.username}."
+
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail=response_error
+        )
+
+    if not user.active:
+        error = (
+            f"Inactive User. User Account has been disabled. User:{form_data.username}"
+        )
+
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=error)
+
+    # Create the access_token:
+    access_token = create_access_token(data={"sub": user.email}, scope="api")
+
+    # get the expiry date time:
+    expiry_date_time = (
+        datetime.utcnow() + timedelta(days=ACCESS_TOKEN_EXPIRE_DAYS)
+    ).strftime("%Y-%m-%d %H:%M:%S")
+
+    # create the token object:
+    token = Token()
+
+    # set the access token, token type, expiry, expiry date time, and success status:
+    token.access_token = access_token
+    token.token_type = "bearer"
+    token.expiry = f"{ACCESS_TOKEN_EXPIRE_DAYS} days(s)"
+    token.expiry_date_time = expiry_date_time
+    token.success = True
+
+    return token
