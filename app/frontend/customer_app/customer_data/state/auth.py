1e354ed3331996a5374d2d46f852cf9eb2e9b769
davidm <dm@salmonbusinesssolutions.com>
2025-02-21 00:11:04 +0300

    feat: enhance signup process with name validation and password strength checks

--- app/frontend/customer_app/customer_data/state/auth.py
+++ app/frontend/customer_app/customer_data/state/auth.py
@@ -4,7 +4,6 @@ import re
 
 import httpx
 import reflex as rx
-from sqlmodel import select
 
 from ..config import settings
 from .base import State
@@ -16,9 +15,11 @@ BASE_URL = f"{settings.fastapi_host}/api/v1"
 class AuthState(State):
     """The authentication state for sign up and login page."""
 
+    name: str
     email: str
     password: str
     confirm_password: str
+    account_created: bool = False
 
     @rx.var(cache=True)
     def invalid_email(self) -> bool:
@@ -27,18 +28,88 @@ class AuthState(State):
             return not re.match(r"[^@]+@[^@]+\.[^@]+", email)
         return False
 
+    @rx.var(cache=True)
+    def invalid_password(self) -> bool:
+        password = self.password.strip()
+
+        if password:
+            # Check if the password is non-empty and has the minimum length
+            if not password or len(password) < 8:
+                return True
+
+            # Check for at least one uppercase letter
+            if not re.search(r"[A-Z]", password):
+                return True
+
+            # Check for at least one lowercase letter
+            if not re.search(r"[a-z]", password):
+                return True
+
+            # Check for at least one digit
+            if not re.search(r"\d", password):
+                return True
+
+        return False
+
     def signup(self):
         """Sign up a user."""
-        with rx.session() as session:
-            if self.password != self.confirm_password:
-                return rx.window_alert("Passwords do not match.")
-            if session.exec(select(User).where(User.email == self.email)).first():
-                return rx.window_alert("Username already exists.")
-            self.user = User(email=self.email, password=self.password)
-            session.add(self.user)
-            session.expire_on_commit = False
-            session.commit()
-            return rx.redirect("/")
+
+        # If name is empty, return an error toast.
+        if not self.name.strip():
+            return rx.toast.error("Name cannot be empty", close_button=True)
+
+        # If email is empty, return an error toast.
+        if not self.email.strip():
+            return rx.toast.error("Email cannot be empty", close_button=True)
+
+        # If password is empty, return an error toast.
+        if not self.password.strip():
+            return rx.toast.error("Password cannot be empty", close_button=True)
+
+        # If confirm password is empty, return an error toast.
+        if not self.confirm_password.strip():
+            return rx.toast.error("Confirm password cannot be empty", close_button=True)
+
+        # If passwords do not match, return an error toast.
+        if self.password != self.confirm_password:
+            return rx.toast.error("Passwords do not match", close_button=True)
+
+        # Show a toast in the UI the account is being created:
+        rx.toast.info("Creating your account..", close_button=True)
+
+        # Create the data obj
+        data = [
+            {
+                "name": self.name,
+                "email": self.email,
+                "password": self.password,
+                "active": True,
+            }
+        ]
+
+        try:
+            response = httpx.post(
+                f"{BASE_URL}/users",
+                json=data,
+            )
+
+        except Exception as e:
+            return rx.toast.error(f"Error: {e}", position="bottom-right")
+
+        # check if the response is a 200 OK
+        if response.status_code == 200:
+            self.account_created = True
+            return rx.toast.success(
+                "Your account has been created successfully. Please sign in.",
+                position="bottom-right",
+            )
+
+        else:
+            print(f"Error occured {response.text=}")
+            error_message = response.json().get(
+                "detail", "Unknown error occurred. Please try again."
+            )
+            return rx.toast.error(error_message, close_button=True)
 
     def login(self):
         """Log in a user."""
