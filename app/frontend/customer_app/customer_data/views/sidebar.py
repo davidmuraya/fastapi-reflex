c42d186e32708b921fc338b7af4666c766c7aad6
davidm <dm@salmonbusinesssolutions.com>
2025-02-20 14:42:45 +0300

    fix: add API Documentation link to sidebar

--- app/frontend/customer_app/customer_data/views/sidebar.py
+++ app/frontend/customer_app/customer_data/views/sidebar.py
@@ -64,6 +64,7 @@ def sidebar_items() -> rx.Component:
         sidebar_item("Projects", "square-library", "/#"),
         sidebar_item("Analytics", "bar-chart-4", "/#"),
         sidebar_item("Messages", "mail", "/contact"),
+        sidebar_item("API Documentation", "book-open", "/api/docs"),
         spacing="1",
         width="100%",
     )
